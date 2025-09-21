6939602680b3fc2a48d0394f46579b4a880e756e
vijayganesh1427 <vijayganesh.1427@gmail.com>
2025-09-21 14:56:49 +0530

    done - exceptions and sys call

--- kernel/exceptions.c
+++ kernel/exceptions.c
@@ -1,4 +1,16 @@
-int main() {
-    return 0;
-}
 //exceptions and sys call
+#include "exceptions.h"
+#include <stdio.h>
+
+void handle_div_zero() {
+    printf("Exception: Divide by zero!\n");
+}
+
+void handle_invalid_mem() {
+    printf("Exception: Invalid memory access!\n");
+}
+
+void syscall_yield() {
+    // Threads voluntarily yield CPU
+    printf("System call: yield\n");
+}
---
+++ kernel/exceptions.h
@@ -0,0 +1,9 @@
+//exceptions and sys call
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+void handle_div_zero();
+void handle_invalid_mem();
+void syscall_yield();
+
+#endif
